400bc08402293ede526e3f35e1b7c4fd2b963185
Blasser-coder <dam14.2022@gmail.com>
2022-11-21 09:52:18 +0100

    Initial Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,10 @@
 package org.example;
-
-import com.sun.org.apache.bcel.internal.Const;
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -13,58 +14,134 @@ import java.util.Scanner;
 public class App 
 {
     public static Scanner sc = new Scanner(System.in);
+    public static Boolean Iterador = true;
+    public static Boolean LogeoMenu = false;
 
-    static void main( String[] args )
-    {
-        Menu();
-        System.out.println("Dime una upcion del menu: ");
-        int Opcion = sc.nextInt();
+    public static FileOutputStream Fos = null;
+    public static FileInputStream Fis = null;
 
-        switch (Opcion){
-            case 1:
-                Opcion1();
-                break;
-            case 2:
-                if (!Constants.FTPconnectionClient.isConnected()){
-                    System.out.println("No se ha definido un servidor FTP al que conectarse");
+    public static void main( String[] args ) {
+        while (Iterador) {
+            Menu();
+            System.out.println("Dime una upcion del menu: ");
+            int Opcion = sc.nextInt();
+
+            switch (Opcion){
+                //OPCION - 1
+                case 1:
+                    Opcion1();
                     break;
-                }
-                break;
-            case 3:
-                if (Constants.FTPconnectionClient.isConnected()){
-                    System.out.println("No se ha definido un servidor FTP al que conectarse");
+                //OPCION - 2
+                case 2:
+                    if (!LogeoMenu){break;}
+                    if (ComprobacionConexionFTP()){break;}
+                    Opcion2();
                     break;
-                }
-                break;
-            case 4:
-                break;
+                //OPCION - 3
+                case 3:
+                    if (!LogeoMenu){break;}
+                    if (ComprobacionConexionFTP()){break;}
+                    Opcion3();
+                    break;
+                //OPCION - 4
+                case 4:
+                    if (!LogeoMenu){break;}
+                    if (ComprobacionConexionFTP()){break;}
+                    Opcion4();
+                    break;
+                //OPCION - 5
+                case 5:
+                    Iterador = false;
+                    break;
+            }
         }
     }
+//"ftp.dlptest.com", 21, "dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu"
 
+//##############################################################################################################
+//#### PROGRAMANDO LAS OPCIONES DEL MENU (PUEDE ESPANDIRSE SI EL JAVIER NO ESTA CONTENTO CON LO QUE LE DOY) ####
+//##############################################################################################################
+
+    //OPCION - 1
     private static void Opcion1(){
         System.out.println("Escriba el nombre/IP del host: ");
         Constants.IP_FTP = sc.next();
         System.out.println("Escriba el Usuario de Inicio de sesion: ");
-        Constants.UserFTP = sc.next();
+        String Usuario = sc.next();
         System.out.println("Escriba la password de Inicio de sesion: ");
-        Constants.PasswordFTP = sc.next();
+        String Pass = sc.next();
         System.out.println("Escriba el puerto de la conexion: ");
         Constants.Puerto = sc.nextInt();
         try{
             Constants.FTPconnectionClient.connect(Constants.IP_FTP, Constants.Puerto);
-            Constants.FTPconnectionClient.user(Constants.UserFTP);
-            Constants.FTPconnectionClient.pass(Constants.PasswordFTP);
+            LogeoMenu = Constants.FTPconnectionClient.login(Usuario, Pass);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    //OPCION - 2
+    public static void Opcion2(){
+        System.out.println("Nombre del fichero que se quiere subir al servidor: ");
+        String FicheroSubida = sc.next();
+        String[] FicheroSplit = FicheroSubida.split(".");
+        try {
+            Fis = new FileInputStream("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //OPCION - 3
+    public static void Opcion3(){
+        System.out.println("Nombre del fichero que se quiere descargar: ");
+        String FicheroDesccarga = sc.next();
+        String[] FicheroSplit = FicheroDesccarga.split(".");
+        try{
+            Fos = new FileOutputStream(FicheroSplit[0] + "." + "_LocalFile" + "." + FicheroSplit[1]);
+            Constants.FTPconnectionClient.retrieveFile(FicheroDesccarga, Fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //OPCION - 4
+    private static void Opcion4(){
+        try{
+            FTPFile[] Files = Constants.FTPconnectionClient.listFiles();
+            String WorkingDirectory = Constants.FTPconnectionClient.printWorkingDirectory();
+            System.out.println("<---------- Current Working Directory: '"+ WorkingDirectory + "' --------->");
+            for (FTPFile f : Files) {
+                System.out.println( f.toFormattedString());
+            }
         } catch (IOException e){
             e.printStackTrace();
         }
     }
 
+//######################################
+//### COMPROBACIONES DE SERVIDOR ETC ###
+//######################################
+    public static boolean ComprobacionConexionFTP(){
+        Boolean b = Constants.FTPconnectionClient.isConnected();
+        if (b) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+//###########################################################
+//#### EL PUTO MENU DE MIERDA NO HACER CASO A ESTO PQ SI ####
+//###########################################################
+
     private static void Menu(){
         System.out.println("<---------Menu-------->");
         System.out.println("1. Logeo en servidor FTP");
-        System.out.println("2. Subida de Archivo/s al servidor FTP");
-        System.out.println("3. Descarga de Archivo/s al servidor FTP");
-        System.out.println("4. Mostrar el arbol de directorios del servidor FTP");
+        if (LogeoMenu) {
+            System.out.println("2. Subida de Archivo/s al servidor FTP");
+            System.out.println("3. Descarga de Archivo/s al servidor FTP");
+            System.out.println("4. Mostrar el arbol de directorios del servidor FTP");
+        }
         System.out.println("5. Salir");
         System.out.println("<---------Fin de Menu-------->");
     }
