13f53477832246e7e7affa650be8de0d637715e5
Blasser-coder <dam14.2022@gmail.com>
2022-11-28 10:02:19 +0100

    Practica Acabada (Ya no hay mas ampliaciones falta funcionalidad)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,11 @@
 package org.example;
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp.FTPReply;
+import org.w3c.dom.xpath.XPathNamespace;
 
 import java.io.*;
+import java.nio.file.Path;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static Scanner sc = new Scanner(System.in);
@@ -22,37 +17,56 @@ public class App
 
     public static void main( String[] args ) {
         while (Iterador) {
-            Menu();
-            System.out.println("Dime una upcion del menu: ");
-            int Opcion = sc.nextInt();
-
-            switch (Opcion){
-                //OPCION - 1
-                case 1:
-                    Opcion1();
-                    break;
-                //OPCION - 2
-                case 2:
-                    if (!LogeoMenu){break;}
-                    if (!ComprobacionConexionFTP()){break;}
-                    Opcion2();
-                    break;
-                //OPCION - 3
-                case 3:
-                    if (!LogeoMenu){break;}
-                    if (!ComprobacionConexionFTP()){break;}
-                    Opcion3();
-                    break;
-                //OPCION - 4
-                case 4:
-                    if (!LogeoMenu){break;}
-                    if (!ComprobacionConexionFTP()){break;}
-                    Opcion4();
-                    break;
-                //OPCION - 5
-                case 5:
-                    Iterador = false;
-                    break;
+            try {
+                Menu();
+                System.out.println("Dime una upcion del menu: ");
+                int Opcion = sc.nextInt();
+                switch (Opcion){
+                    //OPCION - 1
+                    case 1:
+                        Opcion1();
+                        break;
+                    //OPCION - 2
+                    case 2:
+                        if (!LogeoMenu){break;}
+                        if (!ComprobacionConexionFTP()){break;}
+                        Opcion2();
+                        break;
+                    //OPCION - 3
+                    case 3:
+                        if (!LogeoMenu){break;}
+                        if (!ComprobacionConexionFTP()){break;}
+                        Opcion3();
+                        break;
+                    //OPCION - 4
+                    case 4:
+                        if (!LogeoMenu){break;}
+                        if (!ComprobacionConexionFTP()){break;}
+                        Opcion4();
+                        break;
+                    //OPCION - 5
+                    case 5:
+                        if (!LogeoMenu){break;}
+                        if (!ComprobacionConexionFTP()){break;}
+                        Opcion5();
+                        break;
+                    //OPCION - 6
+                    case 6:
+                        Opcion6();
+                        break;
+                    //OPCION - 7
+                    case 7:
+                        if (!LogeoMenu){break;}
+                        if (!ComprobacionConexionFTP()){break;}
+                        Opcion7();
+                        break;
+                    //OPCION - 8
+                    case 8:
+                        Iterador = false;
+                        break;
+                }
+            } catch(IOException e){
+                e.printStackTrace();
             }
         }
     }
@@ -78,9 +92,15 @@ public class App
         } catch (IOException e){
             e.printStackTrace();
         }
+
+        if(LogeoMenu){
+            System.out.println("Inicio de sesion satisfactorio");
+        }else{
+            System.out.println("Error de inicio de sesion en el servidor");
+        }
     }
     //OPCION - 2
-    public static void Opcion2(){
+    private static void Opcion2(){
         Boolean FicheroSubido = false;
         System.out.println("Nombre del fichero que se quiere subir al servidor: ");
         String FicheroSubida = sc.next();
@@ -99,7 +119,7 @@ public class App
     }
 
     //OPCION - 3
-    public static void Opcion3(){
+    private static void Opcion3(){
         System.out.println("Nombre del fichero que se quiere descargar: ");
         String FicheroDesccarga = sc.next();
         String[] FicheroSplit = FicheroDesccarga.split("\\.");
@@ -124,6 +144,73 @@ public class App
             e.printStackTrace();
         }
     }
+    //OPCION - 5
+    private static void Opcion5(){
+        boolean iterator = true;
+        String PathFile = "";
+        while (iterator){
+            System.out.println("Dime la ruta del fichero que quieres eliminar");
+            PathFile = sc.next();
+            File f = new File(PathFile);
+            if (!f.exists()){
+                System.out.println("No se ha encontrado el fichero indicado, intentelo de nuevo");
+            }else{
+                iterator = false;
+            }
+        }
+        boolean DeletedFile = false;
+        try{
+            DeletedFile = Constants.FTPconnectionClient.deleteFile(PathFile);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        if(DeletedFile){
+            System.out.println("Fichero borrado satisfactoriamente...");
+        }else{
+            System.out.println("No se ha podido borrar el fichero...");
+        }
+    }
+    //OPCION - 6
+    private static void Opcion6(){
+        if(LogeoMenu){
+            try {
+                Constants.FTPconnectionClient.disconnect();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            System.out.println("Deslogeo Realizado con exito...");
+        }else{
+            System.out.println("No se ha iniciado sesion en ningun servidor FTP");
+        }
+        LogeoMenu = Constants.FTPconnectionClient.isConnected();
+    }
+    //OPCION - 7
+    private static void Opcion7(){
+        String PathName = "";
+        Boolean iterator = true;
+        Boolean DirectoryChanged = false;
+        try{
+            while(iterator){
+                System.out.println("Dime el nombre del directorio al que quiere cambiarse");
+                PathName = sc.next();
+                File f = new File(PathName);
+                if (!f.exists()){
+                    System.out.println("El directorio no se ha encontrado vuelve a intentarlo");
+                }else{
+                    iterator = false;
+                }
+            }
+            DirectoryChanged = Constants.FTPconnectionClient.changeWorkingDirectory(PathName);
+            if(DirectoryChanged){
+                System.out.println("Directorio Cambiado Satisfactoriamente...");
+            }else{
+                System.out.println("No se ha podido cambiar de directorio, intentelo de nuevo...");
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 
 //######################################
 //### COMPROBACIONES DE SERVIDOR ETC ###
@@ -141,15 +228,21 @@ public class App
 //#### EL PUTO MENU DE MIERDA NO HACER CASO A ESTO PQ SI ####
 //###########################################################
 
-    private static void Menu(){
+    private static void Menu() throws IOException {
         System.out.println("<---------Menu-------->");
+        if(LogeoMenu){
+            System.out.println("|----Current Working Directory: " + Constants.FTPconnectionClient.printWorkingDirectory() + "----|");
+        }
         System.out.println("1. Logeo en servidor FTP");
         if (LogeoMenu) {
             System.out.println("2. Subida de Archivo/s al servidor FTP");
             System.out.println("3. Descarga de Archivo/s al servidor FTP");
             System.out.println("4. Mostrar el arbol de directorios del servidor FTP");
+            System.out.println("5. Eliminar carpeta/fichero del servidor");
+            System.out.println("6. Deslogeo del servidor FTP");
+            System.out.println("7. Cambiar de directorio de trabajo");
         }
-        System.out.println("5. Salir");
+        System.out.println("8. Salir");
         System.out.println("<---------Fin de Menu-------->");
     }
 
